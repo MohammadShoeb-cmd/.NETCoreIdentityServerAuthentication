e3be6a478c65adb40359ba287373e99a1ed17340
MohammadShoeb-cmd <mohsho20@gmail.com>
2020-09-29 16:27:40 +0530

    first commit

---
+++ src/Api/Controllers/IdentityController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IdentityController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+        }
+    }
+}
